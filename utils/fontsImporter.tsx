fbc2701739f0bd00e9737b5c517c67e58974b7b4
SebastianPersonal <sebastian.kolto@gmail.com>
2025-10-07 13:02:08 +0200

    ADD: FontsImporter
    -Importing fonts and exporting font objects which we can use in other components

---
+++ utils/fontsImporter.tsx
@@ -0,0 +1,10 @@
+import { Gabarito, Nunito } from 'next/font/google';
+
+export const gabarito = Gabarito({
+  subsets: ['latin'],
+  weight: ['400', '700', '800'],
+});
+
+export const nunito = Nunito({
+  subsets: ['latin'],
+});
